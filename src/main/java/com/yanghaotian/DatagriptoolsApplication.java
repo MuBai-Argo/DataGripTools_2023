62244f297a5395c474b6f951d47b5e2d0c147720
ArgoM <ArgoMuBai@outlook.com>
2023-11-22 17:13:18 +0800

    a

---
+++ src/main/java/com/yanghaotian/DatagriptoolsApplication.java
@@ -0,0 +1,13 @@
+package com.yanghaotian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagriptoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatagriptoolsApplication.class, args);
+    }
+
+}
